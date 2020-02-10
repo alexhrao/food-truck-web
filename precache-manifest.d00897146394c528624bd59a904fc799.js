e40a812bd7382d372e4d3371c52c022259ce9ee2
Alexander Rao <alexhrao@gmail.com>
2020-02-10 11:53:44 -0500

    Updates

---
+++ precache-manifest.d00897146394c528624bd59a904fc799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f5b136ade530ab06699d0d092f5b54",
+    "url": "/food-truck-web/index.html"
+  },
+  {
+    "revision": "661b8af85ec1e47067e4",
+    "url": "/food-truck-web/static/css/main.852c6d85.chunk.css"
+  },
+  {
+    "revision": "25dcb6a70ca48e666210",
+    "url": "/food-truck-web/static/js/2.f5edaa73.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/food-truck-web/static/js/2.f5edaa73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661b8af85ec1e47067e4",
+    "url": "/food-truck-web/static/js/main.ec9353dc.chunk.js"
+  },
+  {
+    "revision": "bdea23818908cd343125",
+    "url": "/food-truck-web/static/js/runtime-main.da81d0f9.js"
+  }
+]);
